46cc61346c33351e16afc88354bda08b06cae328
Prudhviraj <23397301+PrudhvirajTirumanisetti@users.noreply.github.com>
2026-05-21 00:06:46 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False 
+        # return sorted(s)==sorted(t)
+        hashMapS = defaultdict(int)
+        hashMapT = defaultdict(int)
+        for letter in s:
+            hashMapS[letter]+=1
+        for letter in t:
+            hashMapT[letter]+=1
+        return hashMapT == hashMapS
+
+
+        
